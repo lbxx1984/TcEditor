05291f5d1e4c0cbaa922f283cd6275863fa6c735
lbxx1984 <279641976@qq.com>
2015-11-24 21:23:34 +0800

    import promise finished

--- src/main.js
+++ src/main.js
@@ -14,30 +14,31 @@ define(function (require) {
     var Light = require('./light/main');
     var IO = require('./io/main');
     var FileSystem = require('fs');
+    var routing = new Routing('mouse-cameramove');
 
 
     // 顺序初始化
-    var routing = new Routing('mouse-cameramove');
-    var promise = new Promise(setupFileSystem)
+    setupFileSystem()
         .then(makeWorkingSpace, unsupported)
         .then(enterWorkingSpace, unsupported)
         .then(setupUI, unsupported)
+        .then(setupStage, unsupported)
+        .then(bindEventHandlers, unsupported)
+        .then(displayInformation, unsupported);
 
 
+    function unsupported() {
+        console.log('Your browser does not support TcEditor.');
+    }
 
-    // 绑定全局resize事件
-    window.onresize = function () {
-        if (routing.stage != null) {
-            routing.stage.resize();
-        }
-    };
-
-
-    function setupFileSystem(resolve, reject) {
-        routing.fs = new FileSystem(function (fs) {
-            fs ? resolve() : reject();
+    function setupFileSystem() {
+        return new Promise(function (resolve, reject) {
+            routing.fs = new FileSystem(function (fs) {
+                fs ? resolve() : reject();
+            });
         });
     }
+
     function makeWorkingSpace() {
         return new Promise(function (resolve, reject) {
             routing.fs.md(window.editorKey, function (result) {
@@ -45,6 +46,7 @@ define(function (require) {
             });
         });
     }
+
     function enterWorkingSpace() {
         return new Promise(function (resolve, reject) {
             routing.fs.cd(window.editorKey, function (result) {
@@ -52,6 +54,7 @@ define(function (require) {
             });
         });
     }
+
     function setupUI() {
         var uiElement = React.createElement(App, {
             commandRouting: function () {routing.main.apply(routing, arguments);}
@@ -65,115 +68,106 @@ define(function (require) {
             }
         });
     }
-    function unsupported() {
-        console.log('Your browser does not support TcEditor.');
+
+    function setupStage() {
+        return new Promise(function (resolve, reject) {
+            var stageRefs = routing.ui.refs.containerleft.refs.stage.refs;
+            var stageDom = routing.ui.refs.containerleft.refs.stage.getDOMNode();
+            // 舞台
+            routing.stage = new Stage({
+                container1: stageRefs.cameracontroller.getDOMNode(),
+                container2: stageRefs.stage2d.getDOMNode(),
+                container3: stageRefs.stage3d.getDOMNode()
+            });
+            // 变形器
+            routing.transformer = new Transformer(routing.stage);
+            // 关节变换器
+            routing.morpher = new Morpher(routing.stage);
+            // 灯光系统
+            routing.light = new Light({stage: routing.stage});
+            // 默认灯光
+            var light = new THREE.PointLight(0xffffff, 1.5, 3000);
+            light.position.set(0, 900, 0);
+            routing.light.add(light);
+            // 必成功
+            resolve();
+        });
     }
 
+    function bindEventHandlers() {
+        // 重置鼠标事件参数
+        function cloneMouseEvent(e) {
+            var parent = e.target.parentNode;
+            var result = {
+                clientX: e.clientX,
+                clientY: e.clientY,
+                offsetX: e.offsetX + parent.offsetLeft,
+                offsetY: e.offsetY + parent.offsetTop,
+                target: e.target,
+                button: e.button
+            };
+            return result;
+        }
+        return new Promise(function (resolve, reject) {
+            var containerleft = routing.ui.refs.containerleft;
+            var stageDom = routing.ui.refs.containerleft.refs.stage.getDOMNode();
+            // 变形器事件
+            routing.transformer.onChange = function (mesh) {
+                containerleft.refs.stage.setState({activeMesh: mesh});
+            }
+            // 关节变换器事件
+            routing.morpher.onChange = function (joint) {
+                containerleft.refs.stage.setState({activeJoint: joint});
+            }
+            // 舞台事件
+            stageDom.addEventListener('mousedown', function (e) {
+                routing.mousedown(cloneMouseEvent(e));
+            });
+            stageDom.addEventListener('mouseup', function (e) {
+                routing.mouseup(cloneMouseEvent(e));
+            });
+            stageDom.addEventListener('mouseleave', function (e) {
+                routing.mouseleave(cloneMouseEvent(e));
+            });
+            stageDom.addEventListener('mousemove', function (e) {
+                routing.mousemove(cloneMouseEvent(e));
+            });
+            stageDom.oncontextmenu = function (event) {
+                routing.mouseRightClick(event);
+                return false;
+            };
+            stageDom.onmousewheel = function(event) {
+                routing.stage.zoom(event);
+                event.stopPropagation();
+                return false;
+            };
+            // 全局resize事件
+            window.onresize = function () {
+                if (routing.stage != null) {
+                    routing.stage.resize();
+                }
+            };
+            // 必成功
+            resolve();
+        });
+    }
 
-
-
-
-    // // UI控制回调
-
-
-
-    // // UI初始化回调
-    // function uiLoaded() {
-
-    //     var stageRefs = this.refs.containerleft.refs.stage.refs;
-    //     var stageDom = this.refs.containerleft.refs.stage.getDOMNode();
-
-    //     // 舞台
-    //     routing.stage = new Stage({
-    //         container1: stageRefs.cameracontroller.getDOMNode(),
-    //         container2: stageRefs.stage2d.getDOMNode(),
-    //         container3: stageRefs.stage3d.getDOMNode()
-    //     });
-    //     // 变形器
-    //     routing.transformer = new Transformer(routing.stage);
-    //     // 关节变换器
-    //     routing.morpher = new Morpher(routing.stage);
-    //     // 灯光系统
-    //     routing.light = new Light({stage: routing.stage});
-
-    //     // 默认灯光
-    //     var light = new THREE.PointLight(0xffffff, 1.5, 3000);
-    //     light.position.set(0, 900, 0);
-    //     routing.light.add(light);
-    //     // light = new THREE.PointLight(0xff0000, 1.5, 3000);
-    //     // light.position.set(900, 900, 0);
-    //     // routing.light.add(light);
-    //     // light = new THREE.PointLight(0x0000ff, 1.5, 3000);
-    //     // light.position.set(900, 900, 900);
-    //     // routing.light.add(light);
-    //     // light = new THREE.PointLight(0x00ff00, 1.5, 3000);
-    //     // light.position.set(0, 900, 900);
-    //     // routing.light.add(light);
-
-    //     // 变形器事件
-    //     routing.transformer.onChange = function (mesh) {
-    //         routing.ui.refs.containerleft.refs.stage.setState({activeMesh: mesh});
-    //     }
-
-    //     // 关节变换器事件
-    //     routing.morpher.onChange = function (joint) {
-    //         routing.ui.refs.containerleft.refs.stage.setState({activeJoint: joint});
-    //     }
-
-    //     // 舞台事件
-    //     stageDom.addEventListener('mousedown', function (e) {
-    //         routing.mousedown(cloneMouseEvent(e));
-    //     });
-    //     stageDom.addEventListener('mouseup', function (e) {
-    //         routing.mouseup(cloneMouseEvent(e));
-    //     });
-    //     stageDom.addEventListener('mouseleave', function (e) {
-    //         routing.mouseleave(cloneMouseEvent(e));
-    //     });
-    //     stageDom.addEventListener('mousemove', function (e) {
-    //         routing.mousemove(cloneMouseEvent(e));
-    //     });
-    //     stageDom.oncontextmenu = function (event) {
-    //         routing.mouseRightClick(event);
-    //         return false;
-    //     };
-    //     stageDom.onmousewheel = function(event) {
-    //         routing.stage.zoom(event);
-    //         event.stopPropagation();
-    //         return false;
-    //     };
-
-    //     // 显示信息
-    //     displayInformation();
-
-    //     // 重置鼠标事件参数
-    //     function cloneMouseEvent(e) {
-    //         var parent = e.target.parentNode;
-    //         var result = {
-    //             clientX: e.clientX,
-    //             clientY: e.clientY,
-    //             offsetX: e.offsetX + parent.offsetLeft,
-    //             offsetY: e.offsetY + parent.offsetTop,
-    //             target: e.target,
-    //             button: e.button
-    //         };
-    //         return result;
-    //     }
-    // }
-
-
-    // // 显示信息
-    // // 由于React的Render是异步的，所以这里要判断APP究竟Render完了没有，uiLoaded执行时Routing.ui的值尚不存在
-    // function displayInformation() {
-    //     if (routing.ui == null) {
-    //         setTimeout(displayInformation, 10);
-    //         return;
-    //     }
-    //     // 显示灯光
-    //     routing.ui.refs.containerright.refs.verticallist.refs.lightBox.setState({
-    //         light: routing.light.children
-    //     });
-    // }
+    function displayInformation() {
+        return new Promise(function (resolve, reject) {
+            showLight();
+            function showLight() {
+                if (routing.ui == null) {
+                    setTimeout(showLight, 10);
+                    return;
+                }
+                // 显示灯光
+                routing.ui.refs.containerright.refs.verticallist.refs.lightBox.setState({
+                    light: routing.light.children
+                });
+                resolve();
+            }
+        });
+    }
 
 
 });
