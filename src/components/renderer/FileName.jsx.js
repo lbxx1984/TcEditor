41f3bb11f8d7e76965f09f3d96bc98a3fbb34b9d
lbxx1984 <279641976@qq.com>
2017-07-11 22:58:24 +0800

    update fcui2; add rename function to explorer

--- src/components/renderer/FileName.jsx.js
+++ src/components/renderer/FileName.jsx.js
@@ -11,11 +11,18 @@ define(function (require) {
         onFileNameClick: function (e) {
             this.props.onAction('select', this.props.item);
         },
+        onFileEditClick: function (e) {
+            e.stopPropagation();
+            this.props.onAction('rename', this.props.item);
+        },
         render: function () {
             var item = this.props.item;
             var icon = 'tc-icon ' + (item.isDirectory ? 'icon-folder' : 'icon-file');
             return (
                 <td className="file-name" onClick={this.onFileNameClick}>
+                    <span className="tc-icon icon-edit" onClick={this.onFileEditClick}></span>
+                    <span className="tc-icon icon-cut"></span>
+                    <span className="tc-icon icon-copy"></span>
                     <span className={icon}></span>
                     {item.name}
                 </td>
