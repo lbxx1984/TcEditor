aa8db03744e35673e9731f7e7363000e391dffdd
lihaitao03 <lihaitao03@baidu.com>
2017-08-04 15:54:23 +0800

    update build config

--- src/index.js
+++ src/index.js
@@ -2,22 +2,5 @@
 // 载入less
 import './css/main.less';
 
-// 载入图片
-import '../resource/textures/ash_uvgrid01.jpg';
-import '../resource/textures/ash_uvgrid02.jpg';
-import '../resource/textures/background.png';
-import '../resource/textures/back_ch.png';
-import '../resource/textures/back_en.png';
-import '../resource/textures/bottom_ch.png';
-import '../resource/textures/bottom_en.png';
-import '../resource/textures/font_ch.png';
-import '../resource/textures/font_en.png';
-import '../resource/textures/left_ch.png';
-import '../resource/textures/left_en.png';
-import '../resource/textures/right_ch.png';
-import '../resource/textures/right_en.png';
-import '../resource/textures/top_ch.png';
-import '../resource/textures/top_en.png';
-
 // 启动
 import './main';
---
+++ webpack.dev.config.js
@@ -0,0 +1,77 @@
+
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    devtool: '#source-map',
+    entry: {
+        'js/dep/three': ['three'],
+        'js/dep/react': ['react', 'react-dom'],
+        'js/dep/other': [
+            'raphael',
+            'file-system',
+            'FileSaver',
+            'jszip',
+            'underscore'
+        ],
+        'js/app': './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    /node_modules/,
+                    path.resolve(__dirname, 'dep/three.84.min'),
+                    path.resolve(__dirname, 'dep/react.15.3.1.min'),
+                    path.resolve(__dirname, 'dep/react-dom.15.3.1.min'),
+                    path.resolve(__dirname, 'dep/raphael.2.2.1.min'),
+                    path.resolve(__dirname, 'dep/FileSaver.1.3.3.min'),
+                    path.resolve(__dirname, 'dep/jszip.3.1.3.min')
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'less-loader'])
+            },
+            {
+                test: /\.(gif|jpg|png|html)\??.*$/,
+                loader: 'url-loader?limit=1&name=[path][name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['js/dep/other', 'js/dep/react', 'js/dep/three']
+        }),
+        new ExtractTextPlugin('css/style.css'),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'index.ejs'),
+        })
+    ],
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            'three' : path.resolve(__dirname, 'dep/three.84.min'),
+            'three-lib': path.resolve(__dirname, 'dep/three-lib'),
+            'react': path.resolve(__dirname, 'dep/react.15.3.1.min'),
+            'react-dom': path.resolve(__dirname, 'dep/react-dom.15.3.1.min'),
+            'fcui2': path.resolve(__dirname, 'dep/fcui2/src'),
+            'raphael': path.resolve(__dirname, 'dep/raphael.2.2.1.min'),
+            'file-system': path.resolve(__dirname, 'dep/filesystem.0.0.2'),
+            'FileSaver': path.resolve(__dirname, 'dep/FileSaver.1.3.3.min'),
+            'jszip': path.resolve(__dirname, 'dep/jszip.3.1.3.min'),
+            'underscore': path.resolve(__dirname, 'dep/underscore.1.8.5')
+        }
+    }
+};
