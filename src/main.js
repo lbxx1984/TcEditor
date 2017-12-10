63c9b39537414f15883507992724331d97ab3312
lbxx1984 <279641976@qq.com>
2017-12-10 18:36:17 +0800

    重构src/main.js

--- src/main.js
+++ src/main.js
@@ -3,139 +3,135 @@
  * @author Brian Li
  * @email lbxxlht@163.com
  */
-define(function (require) {
+import ReactDOM from 'react-dom';
+import React from 'react';
+import config from './config';
+import emptyEditorDataset from './emptyEditorDataset';
+import App from './App.jsx';
+import dispatcher from './dispatcher';
+import model from './core/model';
+import hotkey from './core/hotkey';
 
 
-    const THREE = require('three');
-    const ReactDOM = require('react-dom');
-    const React = require('react');
-    const _ = require('underscore');
-
-
-    const emptyEditorDataset = require('./emptyEditorDataset');
-    const config = require ('./config');
-    const App = require('./App.jsx');
-    const dispatcher = require('./dispatcher/index');
-    const model = require('./core/model');
-    const hotkey = require('./core/hotkey');
-
-
-    model.fill(config);
-    model.fill(emptyEditorDataset);
-    model.onChange = function (store) {
-        render(store);
-    };
-    render(model.store);
-
-
-    function render(store) {
-        if (store.path.length) {
-            document.title = config.editorTitle + ' ' +store.path.split('/').pop();
-        }
-        let props = _.extend({}, store, {dispatch: dispatch});
-        ReactDOM.render(React.createElement(App, props), document.getElementById('main'));
+// 渲染页面
+function render(store) {
+    if (store.path.length) {
+        document.title = config.editorTitle + ' ' +store.path.split('/').pop();
     }
+    let props = Object.assign({}, store, {dispatch: dispatch});
+    ReactDOM.render(React.createElement(App, props), document.getElementById('main'));
+}
 
 
-    function dispatch() {
-        if (arguments.length === 0) return;
-        var args = [].slice.apply(arguments);
-        var handler = args.shift();
-        if (typeof handler !== 'string') {
-            args.unshift(handler);
-            handler = typeof handler.type === 'string' ? handler.type : '';
-        }
-        if (typeof dispatcher[handler] === 'function') {
-            return dispatcher[handler].apply(model, args);
-        }
+// 响应交互
+function dispatch() {
+    if (arguments.length === 0) return;
+    var args = [].slice.apply(arguments);
+    var handler = args.shift();
+    if (typeof handler !== 'string') {
+        args.unshift(handler);
+        handler = typeof handler.type === 'string' ? handler.type : '';
     }
-
-
-    function moveCameraByKeyboard(angle, direction) {
-        let stage = JSON.parse(JSON.stringify(model.store.stage));
-        stage.camera3D[angle] += direction;
-        if (angle === 'cameraAngleA') {
-            stage.camera3D[angle] = Math.min(stage.camera3D[angle], 89);
-            stage.camera3D[angle] = Math.max(stage.camera3D[angle], -89);
-        }
-        model.set('stage', stage);
+    if (typeof dispatcher[handler] === 'function') {
+        return dispatcher[handler].apply(model, args);
     }
+}
 
 
-    // 挂接所有快捷键事件
-    hotkey.on('escape', function () {
-        let mesh = model.store.selectedMesh;
-        mesh && mesh.material.setValues({color: mesh.tc.materialColor});
-        model.fill({
-            selectedMesh: null,
-            selectedVector: null,
-            selectedVectorIndex: -1
-        });
-    });
-    hotkey.on('delete', function () {
-        if (!model.store.selectedMesh) return;
-        dispatcher['deleteMesh'].apply(model, [model.store.selectedMesh.uuid]);
-    });
-    hotkey.on('f1', function () {
-        dispatcher['help-hotkey'].apply(model);
-    });
-    hotkey.on('arrowdown', function () {
-        moveCameraByKeyboard('cameraAngleA', -1);
-    });
-    hotkey.on('arrowup', function () {
-        moveCameraByKeyboard('cameraAngleA', 1);
-    });
-    hotkey.on('arrowright', function () {
-        moveCameraByKeyboard('cameraAngleB', 1);
-    });
-    hotkey.on('arrowleft', function () {
-        moveCameraByKeyboard('cameraAngleB', -1);
-    });
-    hotkey.on('ctrl + o', function () {
-        dispatcher['file-open'].apply(model);
-    });
-    hotkey.on('ctrl + s', function () {
-        dispatcher['file-save'].apply(model);
-    });
-    hotkey.on('ctrl + shift + s', function () {
-        dispatcher['file-saveAs'].apply(model);
-    });
-    hotkey.on('ctrl + shift + i', function () {
-        dispatcher['file-import'].apply(model);
-    });
-    hotkey.on('ctrl + shift + e', function () {
-        dispatcher['file-export'].apply(model);
-    });
-    hotkey.on('alt + digit1', function () {
-        dispatcher['view-3d'].apply(model);
-    });
-    hotkey.on('alt + digit2', function () {
-        dispatcher['view-xoz'].apply(model);
-    });
-    hotkey.on('alt + digit3', function () {
-        dispatcher['view-xoy'].apply(model);
-    });
-    hotkey.on('alt + digit4', function () {
-        dispatcher['view-zoy'].apply(model);
-    });
-    hotkey.on('alt + digit5', function () {
-        dispatcher['view-all'].apply(model);
-    });
-    hotkey.on('ctrl + d', function () {
-        dispatcher['tool-pickGeometry'].apply(model);
-    });
-    hotkey.on('ctrl + f', function () {
-        dispatcher['tool-pickJoint'].apply(model);
-    });
-    hotkey.on('ctrl + g', function () {
-        dispatcher['tool-pickLight'].apply(model);
-    });
-    hotkey.on('ctrl + e', function () {
-        dispatcher['camera-move'].apply(model);
-    });
-    hotkey.on('ctrl + r', function () {
-        let handler = model.store.transformer3Dinfo.mode === 'translate'
-            ? 'transformer-3d-mode-rotate' : 'transformer-3d-mode-translate';
-        dispatcher[handler].apply(model);
+// 键盘特殊交互
+function moveCameraByKeyboard(angle, direction) {
+    let stage = JSON.parse(JSON.stringify(model.store.stage));
+    stage.camera3D[angle] += direction;
+    if (angle === 'cameraAngleA') {
+        stage.camera3D[angle] = Math.min(stage.camera3D[angle], 89);
+        stage.camera3D[angle] = Math.max(stage.camera3D[angle], -89);
+    }
+    model.set('stage', stage);
+}
+
+
+// 配置原始数据
+model.fill(config);
+model.fill(emptyEditorDataset);
+// 挂载model事件
+model.onChange = function (store) {
+    render(store);
+};
+// 挂接所有快捷键事件
+hotkey.on('escape', function () {
+    let mesh = model.store.selectedMesh;
+    mesh && mesh.material.setValues({color: mesh.tc.materialColor});
+    model.fill({
+        selectedMesh: null,
+        selectedVector: null,
+        selectedVectorIndex: -1
     });
 });
+hotkey.on('delete', function () {
+    if (!model.store.selectedMesh) return;
+    dispatcher['deleteMesh'].apply(model, [model.store.selectedMesh.uuid]);
+});
+hotkey.on('f1', function () {
+    dispatcher['help-hotkey'].apply(model);
+});
+hotkey.on('arrowdown', function () {
+    moveCameraByKeyboard('cameraAngleA', -1);
+});
+hotkey.on('arrowup', function () {
+    moveCameraByKeyboard('cameraAngleA', 1);
+});
+hotkey.on('arrowright', function () {
+    moveCameraByKeyboard('cameraAngleB', 1);
+});
+hotkey.on('arrowleft', function () {
+    moveCameraByKeyboard('cameraAngleB', -1);
+});
+hotkey.on('ctrl + o', function () {
+    dispatcher['file-open'].apply(model);
+});
+hotkey.on('ctrl + s', function () {
+    dispatcher['file-save'].apply(model);
+});
+hotkey.on('ctrl + shift + s', function () {
+    dispatcher['file-saveAs'].apply(model);
+});
+hotkey.on('ctrl + shift + i', function () {
+    dispatcher['file-import'].apply(model);
+});
+hotkey.on('ctrl + shift + e', function () {
+    dispatcher['file-export'].apply(model);
+});
+hotkey.on('alt + digit1', function () {
+    dispatcher['view-3d'].apply(model);
+});
+hotkey.on('alt + digit2', function () {
+    dispatcher['view-xoz'].apply(model);
+});
+hotkey.on('alt + digit3', function () {
+    dispatcher['view-xoy'].apply(model);
+});
+hotkey.on('alt + digit4', function () {
+    dispatcher['view-zoy'].apply(model);
+});
+hotkey.on('alt + digit5', function () {
+    dispatcher['view-all'].apply(model);
+});
+hotkey.on('ctrl + d', function () {
+    dispatcher['tool-pickGeometry'].apply(model);
+});
+hotkey.on('ctrl + f', function () {
+    dispatcher['tool-pickJoint'].apply(model);
+});
+hotkey.on('ctrl + g', function () {
+    dispatcher['tool-pickLight'].apply(model);
+});
+hotkey.on('ctrl + e', function () {
+    dispatcher['camera-move'].apply(model);
+});
+hotkey.on('ctrl + r', function () {
+    let handler = model.store.transformer3Dinfo.mode === 'translate'
+        ? 'transformer-3d-mode-rotate' : 'transformer-3d-mode-translate';
+    dispatcher[handler].apply(model);
+});
+// 启动页面
+render(model.store);
