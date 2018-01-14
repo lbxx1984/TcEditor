3834ffb6148b868dc84b6899dba08bd9f85a0679
lbxx1984 <279641976@qq.com>
2018-01-14 11:50:33 +0800

    rebuild ToolsBar

--- src/components/ToolsBar.jsx
+++ src/components/ToolsBar.jsx
@@ -3,72 +3,69 @@
  * @author Brian Li
  * @email lbxxlht@163.com
  */
-define(function (require) {
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 
-    var React = require('react');
-    var _ = require('underscore');
-
-
-    var datasourceFilters = {
-        'tool-pickGeometry': function (datasource, controls) {
-            return datasource.map(function (item ,index) {
-                item = _.extend({}, item);
-                var value = item.value.split('-').pop();
-                item.checked = value !== 'space' ? value === controls.mode : controls.space === 'world';
-                return item;
-            });
-        }
-    };
-
-
-    return React.createClass({
-        contextTypes: {
-            dispatch: React.PropTypes.func
-        },
-        // @override
-        getDefaultProps: function () {
-            return {};
-        },
-        // @override
-        getInitialState: function () {
-            return {};
-        },
-        onButtonClick: function (e) {
-            this.context.dispatch(e.target.dataset.uiCmd);
-        },
-        render: function () {
-            var datasource = typeof datasourceFilters[this.props.tool] === 'function'
-                ? datasourceFilters[this.props.tool](this.props.datasource, this.props.controls)
-                : this.props.datasource;
-            return (
-                <div className="tc-tools-bar">
-                    {buttonFactory(datasource, this)}
-                </div>
-            );
-        }
-    });
-
-
-    function buttonFactory(datasource, me) {
-        var result = [];
-        datasource.map(function (item, index) {
-            var iconProps = {
-                key: 'tools-' + index,
-                'data-ui-cmd': item.value,
-                title: item.title,
-                className: 'tc-icon ' + item.icon + (item.checked ? ' selected' : ''),
-                onClick: me.onButtonClick
-            };
-            if (item.hasOwnProperty('color')) {
-                iconProps.style = {
-                    color: item.color
-                };
-            }
-            result.push(<div {...iconProps}></div>);
+const datasourceFilters = {
+    'tool-pickGeometry': (datasource, controls) => {
+        return datasource.map(item=> {
+            item = {...item};
+            const value = item.value.split('-').pop();
+            item.checked = value !== 'space'
+                ? value === controls.mode : controls.space === 'world';
+            return item;
         });
-        return result;
+    }
+};
+
+
+export default class ToolsBar extends Component {
+
+    static contextTypes = {
+        dispatch: PropTypes.func
     }
 
+    static propTypes = {
+        tool: PropTypes.string.isRequired,
+        datasource: PropTypes.array.isRequired,
+        controls: PropTypes.object.isRequired
+    }
 
-});
+    constructor(props) {
+        super(props);
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+    
+    onButtonClick(e) {
+        this.context.dispatch(e.target.dataset.uiCmd);
+    }
+
+    render() {
+        const datasource = typeof datasourceFilters[this.props.tool] === 'function'
+            ? datasourceFilters[this.props.tool](this.props.datasource, this.props.controls)
+            : this.props.datasource;
+        return (
+            <div className="tc-tools-bar">
+                {buttonRenderer(datasource, this)}
+            </div>
+        );
+    }
+}
+
+
+function buttonRenderer(datasource, me) {
+    return datasource.map(function (item, index) {
+        const iconProps = {
+            'data-ui-cmd': item.value,
+            title: item.title,
+            className: 'tc-icon ' + item.icon + (item.checked ? ' selected' : ''),
+            onClick: me.onButtonClick,
+            style: item.hasOwnProperty('color') ? {
+                color: item.color
+            } : null
+        };
+        return <div {...iconProps} key={index}></div>;
+    });
+}
+
