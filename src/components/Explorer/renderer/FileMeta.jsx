3e2f63a578ae4d165e3537e2b7929a643dc391ec
lbxx1984 <279641976@qq.com>
2018-01-22 21:18:16 +0800

    rebuild FileMeta

--- src/components/Explorer/renderer/FileMeta.jsx
+++ src/components/Explorer/renderer/FileMeta.jsx
@@ -3,49 +3,55 @@
  * @author Brian Li
  * @email lbxxlht@163.com
  */
-define(function (require) {
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import util from 'fcui2/core/util';
 
-    var React = require('react');
-    var uiUtil = require('fcui2/core/util');
+function formatSize(n) {
+    const unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB'];
+    let i = 0; 
+    while (n > 1024) {n = n / 1024; i++;}
+    return n.toFixed(i === 0 ? 0 : 2) + unit[i];
+}
 
+export default class FileMeta extends Component {
 
-    function formatSize(n) {
-        var i = 0;
-        var unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB'];
-        while (n > 1024) {n = n / 1024; i++;}
-        return n.toFixed(i === 0 ? 0 : 2) + unit[i];
+    static propTypes = {
+        item: PropTypes.object.isRequired,
+        field: PropTypes.string.isRequired
     }
 
+    constructor(props) {
+        super(props);
+        this.getMeta = this.getMeta.bind(this);
+        this.state = {
+            mtime: 0,
+            size: '-'
+        };
+    }
 
-    return React.createClass({
-        getInitialState: function (e) {
-            return {
-                mtime: 0,
-                size: '-'
-            };
-        },
-        componentDidMount: function () {
-            this.getMeta(this.props.item);
-        },
-        componentWillReceiveProps(nextProps) {
-            if (nextProps.item !== this.props.item) this.getMeta(nextProps.item);
-        },
-        getMeta(item) {
-            var me = this;
-            item.getMetadata(function (meta) {
-                me.setState({
-                    mtime: uiUtil.dateFormat(meta.modificationTime, 'YYYY/MM/DD hh:mm:ss'),
-                    size: me.props.item.isDirectory ? '-' : formatSize(meta.size)
-                });
+    componentDidMount() {
+        this.getMeta(this.props.item);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.item !== this.props.item) this.getMeta(nextProps.item);
+    }
+
+    getMeta(item) {
+        item.getMetadata(meta => {
+            this.setState({
+                mtime: util.dateFormat(meta.modificationTime, 'YYYY/MM/DD hh:mm:ss'),
+                size: this.props.item.isDirectory ? '-' : formatSize(meta.size)
             });
-        },
-        render: function () {
-            return (
-                <td className="file-meta" key={this.props.item.fullPath}>
-                    {this.state[this.props.field]}
-                </td>
-            );
-        }
-    });
+        });
+    }
 
-});
+    render() {
+        return (
+            <td className="file-meta" key={this.props.item.fullPath}>
+                {this.state[this.props.field]}
+            </td>
+        );
+    }
+}
